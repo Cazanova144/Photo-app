cd69d46aa77176d1561d2769c4e189c1f9ce875e
Cazanova144 <cazpians@gmail.com>
2022-03-03 15:07:44 +0100

    Lade in routes

---
+++ routes/albums.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+const albumController = require('../controllers/album_controller');
+const albumValidationRules = require('../validation/album');
+
+/* Get all resources */
+router.get('/', albumController.index);
+
+/* Get a specific resource */
+router.get('/:albumId', albumController.show);
+
+/* Store a new resource */
+router.post('/', albumValidationRules.createRules, albumController.store);
+
+/* Update a specific resource */
+router.put('/:albumId', albumValidationRules.updateRules, albumController.update);
+
+/* Lägg till bild till album */
+router.post('/:albumId/photos', albumValidationRules.createRules, albumController.store);
+
+/* Destroy a specific resource */
+router.delete('/:albumId', albumController.destroy);
+
+module.exports = router;
