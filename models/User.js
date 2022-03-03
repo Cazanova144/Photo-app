3759b5c05cd2e9a1f59e216a8d3a7e2de623866f
Cazanova144 <cazpians@gmail.com>
2022-03-03 13:18:53 +0100

    Lade in hash och salt på user model

--- models/User.js
+++ models/User.js
@@ -1,6 +1,7 @@
 /**
  * User model
  */
+const bcrypt = require('bcrypt');
 
 module.exports = (bookshelf) => {
 	return bookshelf.model('User', {
@@ -10,6 +11,31 @@ module.exports = (bookshelf) => {
 		}, 
 		photos() {
 			return this.hasMany('Photo');
+		}
+	}, {
+		hashSaltRounds: 10,
+
+		async fetchById(id, fetchOptions = {}) {
+			return await new this({ id }).fetch(fetchOptions);
+		},
+
+		async login(username, password) {
+			// Hitta användare med användarnamnet (return false om användaren inte finns)
+			const user = await new this({ username }).fetch({ require: false });
+			if (!user) {
+				return false;
+			}
+			const hash = user.get('password');
+
+			// Hasha lösenordet med saltet från db
+			// Jämför om genererade hashen matchar db-hashen
+			const result = await bcrypt.compare(password, hash);
+			if (!result) {
+				return false;
+			}
+
+			// Om allt funkar, return user
+			return user;
 		}
 	});
 };
