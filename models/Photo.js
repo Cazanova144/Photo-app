227a562d980eb7a2c7c78e5e9388f459d4f39f98
Cazanova144 <cazpians@gmail.com>
2022-03-09 09:40:36 +0100

    Uppdaterade modellen

--- models/Photo.js
+++ models/Photo.js
@@ -5,6 +5,7 @@
 module.exports = (bookshelf) => {
 	return bookshelf.model('Photo', {
 		tableName: 'photos',
+        hidden: ['_pivot_album_id', '_pivot_photo_id'],
         user() {
             return this.belongsTo('User'); // photos.user_id
         },
