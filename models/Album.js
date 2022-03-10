88d2781c388c10deac2fdb8b7222b507991e2f62
Cazanova144 <cazpians@gmail.com>
2022-03-10 18:24:55 +0100

    Fixade så den hämtar info från photos_albums tabellen

--- models/Album.js
+++ models/Album.js
@@ -5,11 +5,12 @@
 module.exports = (bookshelf) => {
 	return bookshelf.model('Album', {
 		tableName: 'albums',
+        hidden: ['_pivot_album_id', '_pivot_photo_id'],
         user() {
             return this.belongsTo('User'); // user.user_id
         },
         photos() {
-            return this.belongsToMany('Photo');
+            return this.belongsToMany('Photo', 'photos_albums');
         }
 	});
 };
